478ed1daf69456d48376cfe4b7a63a6d959b2187
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 19:31:11 -0400

    update EventListener.php

--- src/saturn/EventListener.php
+++ src/saturn/EventListener.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace saturn;
+
+use pocketmine\player\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+class EventListener implements Listener {
+  
+  public function onDamage(EntityDamageEvent $event):void {
+        if(!$event->getEntity() instanceof Player) return;
+        
+        $event->cancel();
+ }
+ 
+ public function onHunger(PlayerExhaustEvent $event):void {
+        $event->cancel();
+ }
+ 
+}
