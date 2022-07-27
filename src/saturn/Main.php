27581f632737cea1b07c9aa3584a2469eb4b955f
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 19:01:16 -0400

    update Main.php

--- src/saturn/Main.php
+++ src/saturn/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace saturn;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class Main Extends PluginBase {
+  
+  use SingletonTrait;
+  
+  public function OnLoad(): void {
+    self::setInstance($this);
+    $this->getLogger()->notice("[!] Saturn Core loaded");
+  }
+  
+  public function OnEnable(): void {
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    
+  }
+  
+}
