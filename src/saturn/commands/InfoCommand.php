e5cc6e3ed9a8b54d0cd35687515325104440770e
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 20:41:08 -0400

    update InfoCommand.php

--- src/saturn/commands/InfoCommand.php
+++ src/saturn/commands/InfoCommand.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace saturn\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class InfoCommand extends Command {
+  
+  public function __construct() {
+    parent::__construct('info', '§l[§a!§r§l] Info Lobby Command');
+    $this->setPermission('info.use');
+  }
+  
+  public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+      if(!$sender->hasPermission("info.use") && !Server::getInstance()->isOp($sender->getName())){
+          $sender->sendMessage("§l[§c!§r§l] §cYou do not have permissions to use this command");
+          return;
+      }
+  }
+}
