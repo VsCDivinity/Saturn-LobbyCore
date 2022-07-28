f5fa2bf463b1ebbbbcf0111940436965f4f01291
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 23:27:47 -0400

    update Scoreboard.php

--- src/saturn/scoreboard/Scoreboard.php
+++ src/saturn/scoreboard/Scoreboard.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace saturn\scoreboard;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+
+class Scoreboard implements Listener {
+  
+  public function OnJoin(PlayerJoinEvent $event): void {
+    $player = $event->getPlayer();
+    self::SendBasicScoreboard($player);
+  }
+  
+  public function sendBasicScoreboard(Player $player): void {
+    $scoreboard = new ScoreboardAPI();
+    $scoreboard->create($player, "§l§dSaturn Network");
+    $scoreboard->setLine($player, 1, "");
+    $scoreboard->setLine($player, 2, "§l§bSaturn.tebex.ui");
+    $scoreboard->setLine($player, 3, "§r");
+  }
+}
