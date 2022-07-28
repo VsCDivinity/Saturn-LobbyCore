509708013302ca2c20a3fd0757abec23f8d33e79
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 23:12:17 -0400

    Add files via upload

---
+++ src/saturn/scoreboard/ScoreboardAPI.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ScoreboardAPI;
+
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
+use pocketmine\player\Player;
+
+//Scoreboard API for only PM4!
+//idk if it will works for PM5
+class ScoreboardAPI
+{
+    public function create(Player $player, string $title): void
+    {
+        $packet = SetDisplayObjectivePacket::create("sidebar", $player->getName(), " {$title} ", "dummy", 0);
+        $player->getNetworkSession()->sendDataPacket($packet);
+    }
+
+    public function remove(Player $player): void
+    {
+        $packet = RemoveObjectivePacket::create($player->getName());
+        $player->getNetworkSession()->sendDataPacket($packet);
+    }
+
+    public function setLine(Player $player, int $line, string $text): void
+    {
+        $entry = new ScorePacketEntry();
+        $entry->scoreboardId = $line;
+        $entry->objectiveName = $player->getName();
+        $entry->score = $line;
+        $entry->type = $entry::TYPE_FAKE_PLAYER;
+        $entry->customName = " {$text} ";
+
+        $packet = SetScorePacket::create(SetScorePacket::TYPE_CHANGE, [$entry]);
+        $player->getNetworkSession()->sendDataPacket($packet);
+    }
+
+    public function removeLine(Player $player, int $line): void
+    {
+        $entry = new ScorePacketEntry();
+        $entry->scoreboardId = $line;
+        $entry->objectiveName = $player->getName();
+        $entry->score = $line;
+        $entry->type = $entry::TYPE_FAKE_PLAYER;
+
+        $packet = SetScorePacket::create(SetScorePacket::TYPE_REMOVE, [$entry]);
+        $player->getNetworkSession()->sendDataPacket($packet);
+    }
+}
