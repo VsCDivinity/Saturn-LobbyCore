2142ae246a65a7f26e39ec08c1bcc30fe3ba8a21
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 23:00:12 -0400

    update BossBar.php

--- src/saturn/bossbar/BossBar.php
+++ src/saturn/bossbar/BossBar.php
@@ -2,6 +2,14 @@
 
 namespace saturn\bossbar;
 
+use pocketmine\scheduler\Task;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use xenialdan\apibossbar\BossBar;
+
+class BossBar extends Listener {
+  
+  $bar = new BossBar()
+  
+  
+}
