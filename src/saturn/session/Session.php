22eb20f0821768a9bf3a655fd69d2b2e3476e66f
imDev68 <84857287+ImDev68@users.noreply.github.com>
2022-07-27 19:37:14 -0500

    update Session.php

--- src/saturn/session/Session.php
+++ src/saturn/session/Session.php
@@ -1,1 +1,45 @@
+<?php
+
+namespace saturn\session;
+
+use saturn\utils\Utils;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\player\GameMode;
+
+class Session implements Listener {
+  
+  /** Session Constructor.
+	*
+	*/
+	
+	 public function onJoin(PlayerJoinEvent $event)
+     {
+     $player = $event->getPlayer();
+    
+    /** Joining Constructor.
+     * 
+     */
+      $event->setJoinMessage(TextFormat::colorize("&7[&dLunaria&f!&7] &f{$player->getName()} join the server"));
+        
+      $player->setGamemode(GameMode::ADVENTURE());
+      $player->setHealth(20);
+        
+      $player->getHungerManager()->setEnabled(false);
+      $player->getHungerManager()->setFood(20);
+        
+      $player->getInventory()->clearAll();
+      $player->getOffHandInventory()->clearAll();
+      $player->getArmorInventory()->clearAll();
+      $player->teleport($player->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+        
+      $player->sendPopup(TextFormat::colorize("&r&eThanks For Playing &dSaturn &fNetwork"));
+    
+    }
+}
+
 
