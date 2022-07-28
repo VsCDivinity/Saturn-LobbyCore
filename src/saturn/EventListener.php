fd8c77c1208f7eb9e11b2aaf7632a9532ea578bf
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-27 21:05:04 -0400

    update EventListener.php funcionará?

--- src/saturn/EventListener.php
+++ src/saturn/EventListener.php
@@ -6,17 +6,23 @@ use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerExhaustEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
 
 class EventListener implements Listener {
   
-    public function onDamage(EntityDamageEvent $event):void {
+   public function onDamage(EntityDamageEvent $event):void {
         if(!$event->getEntity() instanceof Player) return;
         
         $event->cancel();
- }
+   }
  
    public function onHunger(PlayerExhaustEvent $event):void {
         $event->cancel();
- }
+   }
  
+   public function onDrop(PlayerDropItemEvent $event) {
+     $player = $event = getPlayer();
+     $event->cancel();
+     $player->sendMessage("§l[§c!§r§l] §cYou can't throw objects here");
+   }
 }
